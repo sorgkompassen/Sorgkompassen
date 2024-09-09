a62c78c948d28ddcafa96174a3bd1e34c1a943a0
sorgkompassen <annabrkic71@gmail.com>
2024-09-09 15:10:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ window.onscroll = function() {
 
     prevScrollPos = currentScrollPos;
 };
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+  event.preventDefault();  // Prevent the form from submitting in the traditional way
+
+  // Get form values
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  // Create an object with the values to send in the email
+  const templateParams = {
+    name: name,
+    email: email
+  };
+
+  // Send the email using EmailJS
+  emailjs.send("service_kaxyek7", "template_8qdhbsg", templateParams)
+    .then(function(response) {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Tack! Ditt meddelande har skickats.");
+    }, function(error) {
+      console.log("FAILED...", error);
+      alert("Tyvärr gick det inte att skicka ditt meddelande.");
+    });
+});
