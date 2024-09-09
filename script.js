85474ab2562f830561ab6f97b24e7e7e22d8270a
sorgkompassen <annabrkic71@gmail.com>
2024-09-09 17:36:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  emailjs.init("XvCtnf9vApVHPljdT"); // Replace with your actual API key
+
+  document.getElementById('send-button-kontakt').addEventListener('click', function() {
+    // Prevent default form submission
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    emailjs.send("service_kaxyek7", "template_59rljvb", {
+      from_name: name,
+      from_email: email,
+      message: message
+    })
+    .then(function(response) {
+      console.log('Success:', response);
+      alert('Email sent successfully!');
+    }, function(error) {
+      console.log('Failed:', error);
+      alert('Failed to send email.');
+    });
+  });
+});
